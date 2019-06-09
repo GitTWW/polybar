596d60e94088b165bace70d33702794549bf14d2
taowuwen <taowuwen@gmail.com>
2019-06-09 21:03:27 +0800

    update

---
+++ lib/jsoncpp-1.7.7/src/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/tww/polybar/lib/jsoncpp-1.7.7/src
+# Build directory: /home/tww/polybar/lib/jsoncpp-1.7.7/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("lib_json")
+subdirs("jsontestrunner")
+subdirs("test_lib_json")
---
+++ lib/jsoncpp-1.7.7/src/jsontestrunner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tww/polybar/lib/jsoncpp-1.7.7/src/jsontestrunner
+# Build directory: /home/tww/polybar/lib/jsoncpp-1.7.7/src/jsontestrunner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
